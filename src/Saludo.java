4e0ed9c3f97f3705ed6e6a73530f7221a5ffd601
miguelgramuntellmac <miggramar2@alu.edu.gva.es>
2025-12-18 12:29:44 +0100

    Clase saludo añadido saludo

--- src/Saludo.java
+++ src/Saludo.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class Saludo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Dime tu nombre");
+        String nombre = sc.nextLine();
+        System.out.println("Hola " + nombre);
+        sc.close();
+    }
 }
